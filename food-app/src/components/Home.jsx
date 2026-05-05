743a8ea378d10a56de0bf8b412cadf2ce2dde52b
ayushporwal01 <porwalayush953@gmail.com>
2026-05-05 23:37:04 +0530

    Initial commit

--- food-app/src/components/Home.jsx
+++ food-app/src/components/Home.jsx
@@ -67,7 +67,7 @@ export default function Home() {
         setFilteredRestaurant={setFilteredRestaurant}
       />
 
-      <div className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-10">
         {loading ? (
           Array(8)
             .fill("")
