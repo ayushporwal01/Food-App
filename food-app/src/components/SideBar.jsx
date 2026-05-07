8cb69ab19d6b66a93b8a92668aa8c5882967f6bf
ayushporwal01 <porwalayush953@gmail.com>
2026-05-07 10:53:59 +0530

    Initial commit

--- food-app/src/components/SideBar.jsx
+++ food-app/src/components/SideBar.jsx
@@ -3,23 +3,24 @@ import { X } from "react-feather";
 import { motion } from "motion/react";
 
 export default function SideBar({ navLinks, toggleSidebar }) {
-  return;
-  <motion.div
-    initial={{ x: 100, opacity: 0 }}
-    animate={{ x: 0, opacity: 1 }}
-    exit={{ x: 100, opacity: 0 }}
-    transition={{ duration: 0.5 }}
-    className="bg-red-300 fixed top-0 right-0 bottom-0 md:hidden justify-center"
-  >
-    <ul className="flex flex-col items-center gap-y-4 pt-5">
-      {navLinks.map((link) => (
-        <li key={link.path} className="text-lg cursor-pointer">
-          <Link to={link.path}>{link.name}</Link>
-        </li>
-      ))}
-      <button onClick={toggleSidebar} className="md:hidden cursor-pointer">
-        <X size={30} />
-      </button>
-    </ul>
-  </motion.div>;
+  return (
+    <motion.div
+      initial={{ x: 100, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 100, opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      className="bg-red-300 fixed top-0 right-0 bottom-0 md:hidden flex justify-center"
+    >
+      <ul className="flex flex-col items-center gap-y-4 pt-5">
+        {navLinks.map((link) => (
+          <li key={link.path} className="text-lg cursor-pointer">
+            <Link to={link.path}>{link.name}</Link>
+          </li>
+        ))}
+        <button onClick={toggleSidebar} className="md:hidden cursor-pointer">
+          <X size={30} />
+        </button>
+      </ul>
+    </motion.div>
+  );
 }
