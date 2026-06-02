f67eff92e4f746b88fa938e438bf281f7075bdbc
ayushporwal01 <porwalayush953@gmail.com>
2026-06-02 12:24:10 +0530

    Initial commit

--- food-app/src/components/AppLayout.jsx
+++ food-app/src/components/AppLayout.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 
-
-
 export default function AppLayout() {
+  const [userName, setUserName] = useState();
+
+  //authentication
+  useEffect(() => {
+    //Make an api call and send username and password
+    const data = {
+      name: "Ayush Porwal",
+    };
+    setUserName(data.name);
+  });
+
   return (
     <userContext.Provider value={{ loggedInUser: "Ayush Porwal" }}>
       <>
