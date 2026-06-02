7a1648843b32e981e5d6989cbb9c2ce125e74ab8
ayushporwal01 <porwalayush953@gmail.com>
2026-06-02 12:18:57 +0530

    Initial commit

--- food-app/src/components/AppLayout.jsx
+++ food-app/src/components/AppLayout.jsx
@@ -1,11 +1,16 @@
+import userContext from "../utils/userContext";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 
+const { loggedInUser } = useContext(userContext);
+
 export default function AppLayout() {
   return (
-    <>
-      <Header />
-      <Outlet />
-    </>
+    <userContext.Provider value={{ loggedInUser: "Ayush Porwal" }}>
+      <>
+        <Header />
+        <Outlet />
+      </>
+    </userContext.Provider>
   );
 }
