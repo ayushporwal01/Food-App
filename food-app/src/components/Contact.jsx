a7ceb9a90e0926a2b5af688b618e4434f4e3c802
ayushporwal01 <porwalayush953@gmail.com>
2026-06-07 15:26:25 +0530

    Initial Commit

--- food-app/src/components/Contact.jsx
+++ food-app/src/components/Contact.jsx
@@ -1,21 +1,38 @@
+import styles from "./contact.module.css";
+
 export default function Contact() {
   return (
     <div className="mt-10 flex flex-col items-center">
       <h1 className="text-2xl font-semibold text-center">Contact Us</h1>
 
-      <form action="#">
-        <div className="mt-8 space-x-3">
+      <form action="#" className="flex flex-col items-center">
+        <div className={styles.formItems}>
           <label htmlFor="fullName" className="text-xl">
             Full Name
           </label>
-          <input type="text" id="fullName" className="border h-8" />
+          <input type="text" id="fullName" className={styles.formInput} />
         </div>
-        <div className="mt-8 space-x-3">
+
+        <div className={styles.formItems}>
           <label htmlFor="email" className="text-xl">
             Email
           </label>
-          <input type="text" id="email" className="border h-8" />
+          <input type="text" id="email" className={styles.formInput} />
         </div>
+
+        <div className={styles.formItems}>
+          <label htmlFor="phone" className="text-xl">
+            Phone
+          </label>
+          <input type="tel" id="phone" className={styles.formInput} />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-black text-white w-full py-1.5 text-lg rounded-lg mt-5 cursor-pointer"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
