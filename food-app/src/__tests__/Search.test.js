1c8a0a207405f5278b71637cdaa62a1951c39d14
ayushporwal01 <porwalayush953@gmail.com>
2026-06-12 10:13:31 +0530

    Initial Commit

--- food-app/src/__tests__/Search.test.js
+++ food-app/src/__tests__/Search.test.js
@@ -8,39 +8,24 @@ import { BrowserRouter } from "react-router-dom";
 
 beforeEach(() => fetchMock(MOCK_DATA));
 
-const setup = () =>
+  it("Should render Home Component with Search Button", async () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>,
+    );
+
+    const searchBtn = screen.getByTestId("searchBtn");
+
+    expect(searchBtn).toBeInTheDocument();
+  });
+
+it("Should Search ResList for pizza text input", async () => {
   render(
     <BrowserRouter>
       <Home />
     </BrowserRouter>,
   );
-
-it("Should render Home Component with Search", async () => {
-  setup();
-
-  const searchBtn = screen.getByTestId("searchBtn");
-
-  expect(searchBtn).toBeInTheDocument();
-});
-
-it("Should Search ResList for pizza text input", async () => {
-  setup();
-
-  const cardsBeforeSearch = await screen.findAllByTestId("resCard");
-  expect(cardsBeforeSearch).toHaveLength(20);
-
-  const searchBtn = screen.getByTestId("searchBtn");
-  const searchInput = screen.getByTestId("searchInput");
-
-  fireEvent.change(searchInput, { target: { value: "pizza" } });
-  fireEvent.click(searchBtn);
-
-  const cardAfterSearch = screen.getAllByTestId("resCard");
-  expect(cardAfterSearch).toHaveLength(2);
-});
-
-it("Should Search ResList for pizza text input", async () => {
-  setup();
 
   const cardsBeforeSearch = await screen.findAllByTestId("resCard");
   expect(cardsBeforeSearch).toHaveLength(20);
