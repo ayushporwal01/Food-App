3ae150ee8c0fb7cc679d578322cf7266c6721a1a
ayushporwal01 <porwalayush953@gmail.com>
2026-06-12 10:02:52 +0530

    Initial Commit

--- food-app/src/__tests__/Search.test.js
+++ food-app/src/__tests__/Search.test.js
@@ -6,7 +6,7 @@ import "@testing-library/jest-dom";
 import { act } from "react";
 import { BrowserRouter } from "react-router-dom";
 
-
+beforeEach(() => fetchMock(MOCK_DATA));
 
 const setup = () =>
   render(
@@ -21,6 +21,22 @@ it("Should render Home Component with Search", async () => {
   const searchBtn = screen.getByTestId("searchBtn");
 
   expect(searchBtn).toBeInTheDocument();
+});
+
+it("Should Search ResList for pizza text input", async () => {
+  setup();
+
+  const cardsBeforeSearch = await screen.findAllByTestId("resCard");
+  expect(cardsBeforeSearch).toHaveLength(20);
+
+  const searchBtn = screen.getByTestId("searchBtn");
+  const searchInput = screen.getByTestId("searchInput");
+
+  fireEvent.change(searchInput, { target: { value: "pizza" } });
+  fireEvent.click(searchBtn);
+
+  const cardAfterSearch = screen.getAllByTestId("resCard");
+  expect(cardAfterSearch).toHaveLength(2);
 });
 
 it("Should Search ResList for pizza text input", async () => {
