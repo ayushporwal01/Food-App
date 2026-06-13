2477025c4d9275a72a41597ccff743ff06c9e864
ayushporwal01 <porwalayush953@gmail.com>
2026-06-13 23:05:30 +0530

    Initial Commit

--- src/__tests__/Cart.test.js
+++ src/__tests__/Cart.test.js
@@ -13,6 +13,8 @@ import Cart from "../components/Cart";
 beforeEach(() => fetchMock(MOCK_DATA));
 
 const setup = () => {
+  const store = createTestStore(); // new store each time
+
   render(
     <BrowserRouter>
       <Provider store={appStore}>
@@ -28,9 +30,10 @@ it("Should render all restaurant menu categories", async () => {
   setup();
 
   const categories = await screen.findAllByTestId("category");
+
   expect(categories).toHaveLength(11);
 });
-it("Should expand accordion and display menu items on click", async () => {
+it("Should expand accordion and show menu items on click", async () => {
   setup();
 
   const accordionHeader = await screen.findByText("Recommended (17)");
@@ -56,7 +59,9 @@ it("Should add item to cart when add button is clicked and update item count in 
 
   fireEvent.click(addBtn[0]);
 
-  const cartItemText = screen.getByText("Cart (1)");
+  fireEvent.click(addBtn[1]);
+
+  const cartItemText = screen.getByText("Cart (2)");
 
   expect(cartItemText).toBeInTheDocument();
 });
@@ -67,16 +72,36 @@ it("Should add items to cart and reflect those items on cart page", async () => 
 
   fireEvent.click(accordionHeader);
 
-  const menuItems = await screen.findAllByTestId("menu-item");
-
-  expect(menuItems).toHaveLength(17);
-
   const addBtn = screen.getAllByRole("button", { name: "Add" });
 
   fireEvent.click(addBtn[0]);
+
+  fireEvent.click(addBtn[1]);
 
   const cartItems = await screen.findAllByTestId("cart-items");
 
   expect(cartItems).toHaveLength(2);
 });
+it("Should clear cart and show empty cart message", async () => {
+  setup();
 
+  const accordionHeader = await screen.findByText("Recommended (17)");
+
+  fireEvent.click(accordionHeader);
+
+  const addBtn = screen.getAllByRole("button", { name: "Add" });
+
+  fireEvent.click(addBtn[0]);
+
+  fireEvent.click(addBtn[1]);;
+
+  const clearCartBtn = screen.getByRole("button", { name: "Clear Cart" });
+
+  fireEvent.click(clearCartBtn);
+
+  expect(
+    screen.getByText(
+      "Your cart looks empty, add some delicious food to your cart.",
+    ),
+  ).toBeInTheDocument();
+});
